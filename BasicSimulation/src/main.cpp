6b9c94ee2f54fbd320bf82b8e6cdc5d63be61bfe
Daniel Whooley <danieljwhooley@gmail.com>
2025-01-31 13:18:29 -0500

    Added window event handling

--- BasicSimulation/src/main.cpp
+++ BasicSimulation/src/main.cpp
@@ -5,21 +5,26 @@
  * TODO: Hotkeys (a for add, r for remove, h for hit
  */
 int main() {
-	auto window =  sf::RenderWindow(sf::VideoMode({512,512}),"Game");
+	auto window =  sf::RenderWindow(sf::VideoMode({1200,800}),"Game", sf::Style::Close | sf::Style::Titlebar);
 	sf::CircleShape body1(100.f);
 
-    while (window.isOpen()) {
-	    while(const std::optional windowEvent = window.pollEvent()) {
-		    {
-		    	if (windowEvent -> is <sf::Event::Closed>()) {
-		    		window.close();
-		    	}
-		    }
-	    	window.clear();
-	    	window.draw(body1);
-	    	window.display();
-	    }
+	const auto onClose = [&window](const sf::Event::Closed&){
+			window.close();
+	};
 
+	const auto onKeyPressed = [&window](const sf::Event::KeyPressed& keyPressed){
+			switch (keyPressed.scancode) {
+				case sf::Keyboard::Scancode::Escape:
+					window.close();
+					break;
+				}
+	};
+
+	while (window.isOpen()) {
+		window.handleEvents(onClose, onKeyPressed);
+		window.clear();
+		window.draw(body1);
+		window.display();
     }
 
 return 0;
